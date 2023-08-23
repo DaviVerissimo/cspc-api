0afde1574f123a37930e726f776bf9affd0ebb4d
DaviVerissimo <69017602+DaviVerissimo@users.noreply.github.com>
2023-08-23 16:55:21 -0300

    Update CspcApiApplication.java

--- src/main/java/uol/compass/cspcapi/CspcApiApplication.java
+++ src/main/java/uol/compass/cspcapi/CspcApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CspcApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CspcApiApplication.class, args);
+		//SpringApplication.run(CspcApiApplication.class, args);
 	}
 
 }
